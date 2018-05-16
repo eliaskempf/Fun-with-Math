2302ddf7d9fcb588d877218cadbbaf63cb2c01c6
icetube23 <eli2008@gmx.de>
2018-05-16 18:29:55 +0200

    Updated linear algebra api

--- Fun with Math/Matrix.hpp
+++ Fun with Math/Matrix.hpp
@@ -1,28 +1,29 @@
 #pragma once
 
 namespace la {
-	template<typename T>
+	template<typename T = double>
 	class Matrix {
 	private:
 		T * mEntries;
-		int mRows;
-		int mCols;
+		size_t mRows;
+		size_t mCols;
 
 	public:
-		Matrix(int, int);
+		Matrix(int, int, T = 0);
 		~Matrix();
 
-		int rows();
-		int columns();
-		T& entry(int, int);
-		T& operator[](int);
+		int rows() const;
+		int columns() const;
+
+		T operator()(int, int) const;
+		T& operator()(int, int);
 	};
 
 	template<typename T>
-	Matrix<T>::Matrix(int rows, int cols)
+	Matrix<T>::Matrix(int rows, int cols, T defVal)
 		: mCols(cols), mRows(rows) {
 		mEntries = new T[rows * cols];
-		memset(mEntries, 0, rows * cols * sizeof(T));
+		memset(mEntries, defVal, rows * cols * sizeof(T));
 	}
 
 	template<typename T>
@@ -31,26 +32,28 @@ namespace la {
 	}
 
 	template<typename T>
-	int Matrix<T>::rows() {
+	int Matrix<T>::rows() const {
 		return mRows;
 	}
 
 	template<typename T>
-	int Matrix<T>::columns() {
+	int Matrix<T>::columns() const {
 		return mCols;
 	}
 
 	template<typename T>
-	T& Matrix<T>::entry(int i, int j) {
+	T Matrix<T>::operator()(int i, int j) const {
 		if (i > mRows || i < 1 || j > mCols || j < 1) {
 			throw std::out_of_range("Exceeded matrix range.");
 		}
-
 		return mEntries[(i - 1) * mCols + (j - 1)];
 	}
 
 	template<typename T>
-	T& Matrix<T>::operator[](int entry) {
-		return mEntries[rows * mCols + cols];
+	T& Matrix<T>::operator()(int i, int j) {
+		if (i > mRows || i < 1 || j > mCols || j < 1) {
+			throw std::out_of_range("Exceeded matrix range.");
+		}
+		return mEntries[(i - 1) * mCols + (j - 1)];
 	}
 }
--- Fun with Math/Vector.hpp
+++ Fun with Math/Vector.hpp
@@ -1,24 +1,30 @@
 #pragma once
+#include <iostream>
 
 namespace la {
-	template<typename T>
+	template<typename T = double>
 	class Vector {
 	private:
-		int mDimension;
 		T * mEntries;
+		size_t mDimension;
 
 	public:
-		Vector(int);
+		Vector(int, T = 0);
 		~Vector();
-		double getLength();
-		int getDimension();
+
+		double getLength() const;
+		size_t getDimension() const;
+		void normalize();
+
+		T operator[](int) const;
 		T& operator[](int);
 	};
 
 	template<typename T>
-	Vector<T>::Vector(int size) 
+	Vector<T>::Vector(int size, T defVal) 
 		: mDimension(size) {
 		mEntries = new T[size];
+		memset(mEntries, defVal, size * sizeof(T));
 	}
 
 	template<typename T>
@@ -27,21 +33,47 @@ namespace la {
 	}
 
 	template<typename T>
-	double Vector<T>::getLength() {
+	double Vector<T>::getLength() const {
 		double sum = 0;
 		for (int i = 0; i < mDimension; i++) { sum += mEntries[i] * mEntries[i]; }
 		return sqrt(sum);
 	}
 
 	template<typename T>
-	int Vector<T>::getDimension() {
+	size_t Vector<T>::getDimension() const {
 		return mDimension;
+	}
+
+	template<typename T>
+	void Vector<T>::normalize() {
+		double norm = this->getLength();
+		std::cout << norm << std::endl;
+		for (size_t i = 0; i < mDimension; i++) {
+			mEntries[i] /= norm;
+		}
+	}
+
+	/*
+	void Vector<int>::normalize() {
+		Vector<double> v(mDimension);
+		double norm = this->getLength();
+		for (size_t i = 0; i < mDimension; i++) {
+			v[i] = mEntries[i] / norm;
+		}
+	}
+	*/
+	template<typename T>
+	T Vector<T>::operator[](int index) const {
+		if (index >= mDimension) {
+			throw std::out_of_range("Exceeded vector range.");
+		}
+		return mEntries[index];
 	}
 
 	template<typename T>
 	T& Vector<T>::operator[](int index) {
 		if (index >= mDimension) {
-			throw std::out_of_range("Vector index out of range.");
+			throw std::out_of_range("Exceeded vector range.");
 		}
 		return mEntries[index];
 	}
